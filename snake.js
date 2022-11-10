60d6122b86fff2240473e7fed67ae7d55334a2dc
Chirag Jain <chiragjain0406@gmail.com>
2022-11-10 23:34:00 +0530

    dark mode added in snake-game

--- snake.js
+++ snake.js
@@ -193,3 +193,17 @@ function keyDown(event) {
 }
 
 drawGame();
+
+// darkmode
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.querySelector('body').
+        classList.add('active');
+    }else{
+        document.querySelector('body').
+        classList.remove('active');
+    }
+}
